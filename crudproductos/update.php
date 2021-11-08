550fd6fd86cc65878190b26c16373a9418477bf5
lpcruz <lpcruz16@misena.edu.co>
2021-11-07 22:45:18 -0500

    cambiosproveedor

--- crudproductos/update.php
+++ crudproductos/update.php
@@ -3,11 +3,11 @@
 include("conexion.php");
 $con=conectar();
 
-$id_categorias=$_POST['id_categorias'];
-$id_productos=$_POST['id_productos'];
-$nombrep=$_POST['nombrep'];
-$stock=$_POST['stock'];
-$valorunitario=$_POST['valorunitario'];
+$id_categorias  =$_POST['id_categorias'];
+$id_productos   =$_POST['id_productos'];
+$nombrep        =$_POST['nombrep'];
+$stock          =$_POST['stock'];
+$valorunitario  =$_POST['valorunitario'];
 
 $sql="UPDATE crud_product SET  id_categorias='$id_categorias',nombrep='$nombrep',stock='$stock',valorunitario='$valorunitario' WHERE id_productos='$id_productos'";
 $query=mysqli_query($con,$sql);
--- interproductos.php
+++ interproductos.php
@@ -27,15 +27,15 @@ echo"<a href='../ProyectoWomenStore/modelo/salir.php'>SALIR </a> ";
 <br>
 <nav>
 <ul class="menu1">
-<li ><a href="../ProyectoWomenStore/interfazindex.php"><b>INICIO</b></I></a> </li>
-<li ><a href="../ProyectoWomenStore/interproductos.php"><b>PRODUCTOS</b> </a> </li>
-<li ><a href="../ProyectoWomenStore/intercategorias.php"><b>CATEGORIAS</b> </a> </li>
-<li ><a href="../ProyectoWomenStore/intercontacto.php"><b>CONTACTO</b> </a> </li>
-<li ><a href="../ProyectoWomenStore/paginaprin.php"><b>MICUENTA</b> </a> </li>
-<li ><a href="../ProyectoWomenStore/interellos.php"><b>ELLOS</b> </a> </li>
-<li><a href="../ProyectoWomenStore/intercarro.php"><div class="carro"><i class="fas fa-shopping-cart"></i></div></a> </li>
+<li ><a href="../ProyectoWomenStore/index.html"><b>INICIO</b></I></a> </li>
+<li ><a href="../ProyectoWomenStore/productos.html"><b>PRODUCTOS</b> </a> </li>
+<li ><a href="../ProyectoWomenStore/categorias.html"><b>CATEGORIAS</b> </a> </li>
+<li ><a href="../ProyectoWomenStore/contacto.html"><b>CONTACTO</b> </a> </li>
+<li ><a href="../ProyectoWomenStore/ellos.html"><b>ELLOS</b> </a> </li>
+<li ><a href="../ProyectoWomenStore/cuenta.html"><b>CUENTA</b> </a> </li>
+<li><a href="../ProyectoWomenStore/carro.html"><div class="carro"><i class="fas fa-shopping-cart"></i></div></a> </li>
 </ul>
-</nav> 
+</nav>
 </header>
 <center>
 <table>
@@ -48,6 +48,169 @@ echo"<a href='../ProyectoWomenStore/modelo/salir.php'>SALIR </a> ";
 
 	
 	<h2>Click En Las Imagenes</h2>
+
+  <!-- inicio de cajas-> caja1 ------------------------------------------------------------->
+  <div class="galeria">
+    <div class="foto">
+      <img src="img/1.jpg" alt="">
+    </div>
+    <div class="pie">
+      <h5>Polvos Compacto Marca DAZZLE <br>
+      $10.999</h5>
+    </div>
+  </div>
+  <!-- caja2 ------------------------------------------------->
+  <div class="galeria">
+    <div class="foto">
+      <img src="img/cabello1.jpg" alt="">
+    </div>
+    <div class="pie">
+      <h5>SHAMPOO  LOREAL ELVIVE Rizos Definidos
+      $17.899</h5>
+    </div>
+  </div>
+  <!-- caja3---------------------------------------------------------- -->
+  <div class="galeria">
+    <div class="foto">
+      <img src="img/crema2.jpg" alt="">
+    </div>
+    <div class="pie">
+      <h5>Crema corporal <br>
+      $17.899</h5><br>
+    </div>
+  </div>
+  <!-- caja4------------------------------------------------- -->
+  <div class="galeria">
+    <div class="foto">
+      <img src="img/esmaltes.jpg" alt="">
+    </div>
+    <div class="pie">
+      <h5>Esmaltes EVON<br>
+      $4.999</h5><br>
+    </div>
+  </div>
+	  <!-- caja5------------------------------------------------- -->
+  <div class="galeria">
+    <div class="foto">
+      <img src="img/maquillaje.jpg" alt="">
+    </div>
+    <div class="pie">
+      <h5></h5>
+      <p><a href="">$17.899</a></p>
+    </div>
+  </div>
+	  <!-- caja6------------------------------------------------- -->
+  <div class="galeria">
+    <div class="foto">
+      <img src="img/solaru.jpg" alt="">
+    </div>
+    <div class="pie">
+    <h5>Protector Solar SUNDAY
+      $45.000
+    </h5>
+     
+    </div>
+  </div>
+	  <!-- caja7------------------------------------------------- -->
+  <div class="galeria">
+    <div class="foto">
+      <img src="img/perfume12.jpg" alt="">
+    </div>
+    <div class="pie">
+      <p>FRAGANCIAS </p>
+      <p><a href="">Ver mas...</a></p>
+    </div>
+  </div>
+	  <!-- caja4------------------------------------------------- -->
+  <div class="galeria">
+    <div class="foto">
+      <img src="img/tratamientos.jpg" alt="">
+    </div>
+    <div class="pie">
+      <p>TRATAMIENTOS</p>
+      <p><a href="">Ver mas...</a></p>
+    </div>
+  </div>
+   <!-- inicio de cajas-> caja1 ------------------------------------------------------------->
+  <div class="galeria">
+    <div class="foto">
+      <img src="img/depilacion1.jpg" alt="">
+    </div>
+    <div class="pie">
+      <p>Depilacion</p>
+      <p><a href="">Ver mas...</a></p>
+    </div>
+  </div>
+  <!-- caja2 ------------------------------------------------->
+  <div class="galeria">
+    <div class="foto">
+      <img src="img/cabello1.jpg" alt="">
+    </div>
+    <div class="pie">
+      <p>CUIDADO DEL CABELLO</p>
+      <p><a href="">Ver mas...</a></p>
+    </div>
+  </div>
+  <!-- caja3---------------------------------------------------------- -->
+  <div class="galeria">
+    <div class="foto">
+      <img src="img/crema2.jpg" alt="">
+    </div>
+    <div class="pie">
+      <p>CUIDADO CORPORAL</p>
+      <p><a href="">Ver mas...</a></p>
+    </div>
+  </div>
+  <!-- caja4------------------------------------------------- -->
+  <div class="galeria">
+    <div class="foto">
+      <img src="img/esmaltes.jpg" alt="">
+    </div>
+    <div class="pie">
+      <p>MANICURA Y PEDICURA </p>
+      <p><a href="">Ver mas...</a></p>
+    </div>
+  </div>
+	  <!-- caja5------------------------------------------------- -->
+  <div class="galeria">
+    <div class="foto">
+      <img src="img/maquillaje.jpg" alt="">
+    </div>
+    <div class="pie">
+      <p>MAQUILLAJE </p>
+      <p><a href="">Ver mas...</a></p>
+    </div>
+  </div>
+	  <!-- caja6------------------------------------------------- -->
+  <div class="galeria">
+    <div class="foto">
+      <img src="img/solaru.jpg" alt="">
+    </div>
+    <div class="pie">
+      <p>PROTECCION SOLAR</p>
+      <p><a href="">Ver mas...</a></p>
+    </div>
+  </div>
+	  <!-- caja7------------------------------------------------- -->
+  <div class="galeria">
+    <div class="foto">
+      <img src="img/perfume12.jpg" alt="">
+    </div>
+    <div class="pie">
+      <p>FRAGANCIAS </p>
+      <p><a href="">Ver mas...</a></p>
+    </div>
+  </div>
+	  <!-- caja4------------------------------------------------- -->
+  <div class="galeria">
+    <div class="foto">
+      <img src="img/tratamientos.jpg" alt="">
+    </div>
+    <div class="pie">
+      <p>TRATAMIENTOS</p>
+      <p><a href="">Ver mas...</a></p>
+    </div>
+  </div>
 	</center>
 	<a href="../ProyectoWomenStore/index.html">Inicio</a>
 <ul class="menu3">
